f102f7e875697b30771a8efddeb96e7efcc35484
muhammadismailkhan0009 <muhammadismailkhan0009@gmail.com>
2025-06-10 13:18:11 +0500

    - refactored code

--- src/actions/scraper/runScraper.ts
+++ src/actions/scraper/runScraper.ts
@@ -2,7 +2,7 @@ import { DataExchangeService } from "services/DataExchangeService";
 import { scrapePost } from "./scrapePost";
 import { storeGeneratedComment } from "./storeGeneratedComment";
 
-export  async function runScraper() {
+export async function runScraper() {
     let scrapedPost = await scrapePost();
     let generatedComment = await new DataExchangeService().sendPostToBackend(scrapedPost, "");
     storeGeneratedComment(generatedComment);
